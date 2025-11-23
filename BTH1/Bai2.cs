ebaf71b131cb83b86bb1eef5f3a358befb169e9d
dobachien <bachiendo@gmail.com>
2025-11-23 19:01:13 +0700

    Update Bai2.cs

--- BTH1/Bai2.cs
+++ BTH1/Bai2.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH_Bai2
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKQ_Click(object sender, EventArgs e)
+        {
+            string hoten = this.txtHoTen.Text.Trim();
+            if (this.rad1.Checked == true)
+                txtKQ.Text = hoten.ToLower();
+            if (this.rad2.Checked == true)
+                txtKQ.Text = hoten.ToUpper();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtHoTen.Clear();
+            this.txtKQ.Clear();
+            this.rad1.Checked = true;
+            this.txtHoTen.Focus();
+
+        }
+    }
+}
 
