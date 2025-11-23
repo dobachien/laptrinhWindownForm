4ce92f5d6ac23e8ba8b430acfa501b85bf088f63
dobachien <bachiendo@gmail.com>
2025-11-23 19:16:37 +0700

    Update BaiMau.cs

--- BTH1/BaiMau.cs
+++ BTH1/BaiMau.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH1_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(txtN.Text);
+            int Tong = 0;
+            for (int i = 0; i <= N; i++)          
+            {
+                Tong += i;
+            }
+            txtTong.Text=Tong.ToString();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
 
